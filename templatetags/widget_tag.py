c41e1c04dfdee8d040ea102e5b3e2f3f72457917
Steve Pulec <spulec@gmail.com>
2011-03-06 23:54:49 -0500

    better design

--- templatetags/widget_tag.py
+++ templatetags/widget_tag.py
@@ -3,11 +3,12 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag("dashboard/widget_block_head.html")
-def widget_tag_head(widget):
+def widget_tag_head(widget, fullscreen=0):
     #widget_data, time_intervals = widget.data_list()
     return {
             'widget': widget,
             'model_name': widget.get_model_display(),
+            'fullscreen': fullscreen,
             #'time_intervals': time_intervals,
             #'data_points': widget_data,
         }
