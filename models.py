2b2fa6e10a4e69a2830c803c9b063b35a45d205f
Steve Pulec <spulec@gmail.com>
2011-03-20 23:43:41 -0400

    fixed ticks and cleaned up dashboard

--- models.py
+++ models.py
@@ -84,37 +84,37 @@ class DashboardWidget(models.Model):
         
         if self.time_period == 'DA':
             time_range = [today + datetime.timedelta(minutes=10*x) for x in range(0, now.hour*6 + now.minute/10)]
-            return time_range, now.hour/4
+            return time_range, 'hour' #now.hour/4
         elif self.time_period == '24':
             time_range = [now - datetime.timedelta(minutes=10*x) for x in range(0, 24 * 6)]
             time_range.reverse()
-            return time_range, 6
+            return time_range, 'hour' #6
         elif self.time_period == 'WE':
             day_of_week = calendar.weekday(now.year, now.month, now.day)
             first_week_day = today - datetime.timedelta(days=day_of_week)
             time_range = [first_week_day + datetime.timedelta(hours=x) for x in range(0, day_of_week * 24)]
-            return time_range, 7
+            return time_range, 'day' #7
         elif self.time_period == '7D':
             time_range = [now - datetime.timedelta(hours=x) for x in range(0, now.hour + 24 * 6)]
             time_range.reverse()
-            return time_range, 7
+            return time_range, 'day' #7
         elif self.time_period == 'MO':
             first_month_day = datetime.datetime(now.year, now.month, 1)
             time_range = [first_month_day + datetime.timedelta(days=x) for x in range(0, now.day)]
-            return time_range, now.day
+            return time_range, 'day' #now.day
         elif self.time_period == '30':
             time_range = [now - datetime.timedelta(days=x) for x in range(0, 30)]
             time_range.reverse()
-            return time_range, 30
+            return time_range, 'day' #30
         elif self.time_period == 'YR':
             first_year_day = datetime.datetime(now.year, 1, 1)
             day_of_year = now.timetuple().tm_yday
             time_range = [first_year_day + datetime.timedelta(days=10 * x) for x in range(0, day_of_year/10)]
-            return time_range, day_of_year / 10
+            return time_range, 'monht' #day_of_year / 10
         elif self.time_period == '36':
             time_range = [now - datetime.timedelta(days=365) for x in range(0, 365)]
             time_range.reverse()
-            return time_range, 365
+            return time_range, 'month' #365
         """
         TODO
         ('AT', 'All Time'),
