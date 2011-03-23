f53bdf7b664862dc3ece7b0c39e58cac0b9470c8
Steve Pulec <spulec@gmail.com>
2011-03-22 23:13:15 -0400

    cleaned up time periods

--- models.py
+++ models.py
@@ -2,7 +2,9 @@ import ast
 import calendar
 import datetime
 
-from django.db import models
+from django.conf import settings
+from django.db import models, DEFAULT_DB_ALIAS
+from django.db.models import Count
 
 def get_model_choices():
     apps = [app for app in models.get_apps()]
@@ -23,6 +25,19 @@ TIME_PERIOD_CHOICES = (
     ('36', '365 Days'),
     ('AT', 'All Time'),
 )
+
+ENGINE_MODULES = {
+    'django.db.backends.postgresql_psycopg2': 'postgresql_psycopg2',
+    'django.db.backends.sqlite3': 'sqlite3',
+    'django.db.backends.mysql': 'mysql',
+    'django.db.backends.oracle': 'oracle',
+    'sql_server.pyodbc': 'sql_server.pyodbc', #django-pyodbc
+    'sqlserver_ado': 'sql_server.pyodbc', #django-mssql
+    'django.contrib.gis.db.backends.postgis': 'postgresql_psycopg2',
+    'django.contrib.gis.db.backends.spatialite': 'sqlite3',
+    'django.contrib.gis.db.backends.mysql': 'mysql',
+    'django.contrib.gis.db.backends.oracle': 'oracle',
+}
 
 LOADING_IMG_HEIGHT = 19
 LOADING_IMG_WIDTH = 220
@@ -59,22 +74,40 @@ class DashboardWidget(models.Model):
         fields = self.get_class()._meta.local_fields
         return [(field.name, field.name) for field in fields if field.__class__.__name__ in ['DateField', 'DateTimeField']]
 
+    def get_db_time_formatter(self):
+        engine = settings.DATABASES[DEFAULT_DB_ALIAS]['ENGINE']
+        alias = ENGINE_MODULES.get(engine, None)
+        if alias == 'sqlite3':
+            return "strftime"
+        elif alias == 'mysql':
+            return 'DATE_FORMAT'
+        else:
+            NotImplemented
+
     def data_points(self):
         filter_dict = ast.literal_eval(self.filter_dict) if self.filter_dict else {}
         return self.get_class().objects.filter(**filter_dict).order_by(self.datetime_field)
     
     def data_list(self):
-        data_array = []
-        # User.objects.extra({'date_created': "date(date_joined)"}).values('date_created').annotate(created_count=Count('id'))
+        data_map = {}
         
-        time_range, time_interval_count = self.get_time_range()
+        time_range, time_filter, time_interval = self.get_time_range()
+        select_data = {"datetime": """%s('%s', last_login)""" % (self.get_db_time_formatter(), time_filter)}
+        date_filter = {str("%s__range" % self.datetime_field): (time_range[0], time_range[-1])}
+        time_range = [time.strftime("%s") for time in time_range]
         
-        points = self.data_points()
         for index, curr_time in enumerate(time_range):
             if index + 1 == len(time_range): continue
-            date_filter = {str("%s__range" % self.datetime_field): (curr_time, time_range[index+1])}
-            data_array.append((str(curr_time), points.filter(**date_filter).count()))
-        return data_array, time_interval_count
+            data_map[curr_time] = 0
+
+        points = self.data_points().filter(**date_filter).extra(select=select_data).values('datetime').annotate(count=Count('id')).order_by()
+        for point in points:
+            point_datetime = datetime.datetime.strptime(point['datetime'], time_filter.replace("%%", "%"))
+            data_map[point_datetime.strftime("%s")] = point['count']
+        
+        data_array = data_map.items()
+        data_array.sort()
+        return data_array, time_interval
     
     def get_time_range(self):
         now = datetime.datetime.now()
@@ -84,37 +117,45 @@ class DashboardWidget(models.Model):
         
         if self.time_period == 'DA':
             time_range = [today + datetime.timedelta(minutes=10*x) for x in range(0, now.hour*6 + now.minute/10)]
-            return time_range, 'hour' #now.hour/4
+            time_filter = "%%m/%%d/%%Y %%H %%M"
+            return time_range, time_filter, 'minute' #now.hour/4
         elif self.time_period == '24':
             time_range = [now - datetime.timedelta(minutes=10*x) for x in range(0, 24 * 6)]
             time_range.reverse()
-            return time_range, 'hour' #6
+            time_filter = "%%m/%%d/%%Y %%H %%M"
+            return time_range, time_filter, 'minute' #6
         elif self.time_period == 'WE':
             day_of_week = calendar.weekday(now.year, now.month, now.day)
             first_week_day = today - datetime.timedelta(days=day_of_week)
-            time_range = [first_week_day + datetime.timedelta(hours=x) for x in range(0, day_of_week * 24)]
-            return time_range, 'day' #7
+            time_range = [first_week_day + datetime.timedelta(hours=x) for x in range(0, now.hour + day_of_week * 24)]
+            time_filter = "%%m/%%d/%%Y %%H"
+            return time_range, time_filter, 'hour' #7
         elif self.time_period == '7D':
-            time_range = [now - datetime.timedelta(hours=x) for x in range(0, now.hour + 24 * 6)]
+            time_range = [now - datetime.timedelta(hours=x) for x in range(0, 24 * 7)]
             time_range.reverse()
-            return time_range, 'day' #7
+            time_filter = "%%m/%%d/%%Y %%H"
+            return time_range, time_filter, 'hour' #7
         elif self.time_period == 'MO':
             first_month_day = datetime.datetime(now.year, now.month, 1)
             time_range = [first_month_day + datetime.timedelta(days=x) for x in range(0, now.day)]
-            return time_range, 'day' #now.day
+            time_filter = "%%m/%%d/%%Y"
+            return time_range, time_filter, 'day' #now.day
         elif self.time_period == '30':
             time_range = [now - datetime.timedelta(days=x) for x in range(0, 30)]
             time_range.reverse()
-            return time_range, 'day' #30
+            time_filter = "%%m/%%d/%%Y"
+            return time_range, time_filter, 'day' #30
         elif self.time_period == 'YR':
             first_year_day = datetime.datetime(now.year, 1, 1)
             day_of_year = now.timetuple().tm_yday
             time_range = [first_year_day + datetime.timedelta(days=10 * x) for x in range(0, day_of_year/10)]
-            return time_range, 'monht' #day_of_year / 10
+            time_filter = "%%m/%%Y"
+            return time_range, time_filter, 'month' #day_of_year / 10
         elif self.time_period == '36':
             time_range = [now - datetime.timedelta(days=365) for x in range(0, 365)]
             time_range.reverse()
-            return time_range, 'month' #365
+            time_filter = "%%m/%%Y"
+            return time_range, time_filter, 'month' #365
         """
         TODO
         ('AT', 'All Time'),
