bcfe41a4269547e31f7c6c19dcf90ef54cb8af57
steve <steve@yipit.com>
2011-05-08 18:04:12 -0400

    Bumped to version 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-curator',
-    version='0.1',
+    version='0.1.1',
     author='Steve Pulec',
     author_email='spulec@gmail.com',
     url='http://github.com/spulec/django-curator',
